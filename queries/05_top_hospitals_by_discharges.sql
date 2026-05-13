e9b40a25c5b988e614477459418503a0d45c5496
pgarlap <poojithagarlapati55@gmail.com>
2026-05-12 21:37:32 -0400

    Update 05_top_hospitals_by_discharges.sql

--- queries/05_top_hospitals_by_discharges.sql
+++ queries/05_top_hospitals_by_discharges.sql
@@ -1,1 +1,11 @@
-
+-- Query 5: Top 10 hospitals with highest number of discharges
+SELECT field1 AS Facility_Name,
+       field3 AS State,
+       field4 AS Condition,
+       CAST(field5 AS INTEGER) AS Num_Discharges
+FROM FY_2026_Hospital_Readmissions_Reduction_Program_Hospital
+WHERE field1 != 'Facility Name'
+AND field5 != 'N/A'
+AND field5 != ''
+ORDER BY CAST(field5 AS INTEGER) DESC
+LIMIT 10;
