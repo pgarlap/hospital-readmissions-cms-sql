ba68609d32672e041c7db45a594d0d011c57bea5
pgarlap <poojithagarlapati55@gmail.com>
2026-05-12 21:36:58 -0400

    Update 04_excess_readmission_by_condition.sql

--- queries/04_excess_readmission_by_condition.sql
+++ queries/04_excess_readmission_by_condition.sql
@@ -1,1 +1,9 @@
-
+-- Query 4: Which condition has highest excess readmissions
+SELECT field4 AS Condition,
+       ROUND(AVG(CAST(field7 AS REAL)), 4) AS Avg_Excess_Ratio
+FROM FY_2026_Hospital_Readmissions_Reduction_Program_Hospital
+WHERE field1 != 'Facility Name'
+AND field7 != 'N/A'
+AND field7 != ''
+GROUP BY field4
+ORDER BY Avg_Excess_Ratio DESC;
